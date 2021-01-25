4828b0ed49a4b4af251ea93b80188fb5f843b24b
Ranieli <ranielisilveira@hotmail.com>
2021-01-25 19:15:03 -0300

    feat: created delete and restore deault dialogs

---
+++ components/dialogs/RestoreDialog.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-dialog v-model="show" persistent width="500" @keydown.esc="show = false">
+    <v-card>
+      <v-card-title class="grey lighten-2">
+        {{ title }}
+      </v-card-title>
+      <v-card-text class="headline pt-4">
+        {{ item }}
+      </v-card-text>
+      <v-card-text class="pt-2 pb-2">
+        <v-alert v-if="info" border="top" color="red lighten-2" dark>
+          {{ info }}
+        </v-alert>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="blue-grey white--text" @click="show = false"
+          >Cancelar</v-btn
+        >
+        <v-spacer></v-spacer>
+        <v-btn color="red white--text" @click="confirm">Confirmar</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+<script>
+export default {
+  name: 'DeleteDialog',
+  props: {
+    title: {
+      type: String,
+      default: 'Remover o item selecionado?',
+    },
+    item: {
+      type: String,
+      default: '',
+    },
+    info: {
+      type: String,
+      default: null,
+    },
+  },
+  data: () => ({
+    show: false,
+  }),
+  methods: {
+    showDialog() {
+      this.show = true
+    },
+    hideDialog() {
+      this.show = false
+    },
+    confirm() {
+      this.$emit('confirm-remove')
+    },
+  },
+}
+</script>
